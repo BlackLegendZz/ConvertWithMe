66d6dea654eeb2d7786dbd20ea7295103a9d62a8
Jananas <jan.gellbach@gmail.com>
2024-12-05 22:49:05 +0100

    adding video and audio formats

---
+++ ConvertWithMe.Core/Definitions/AudioFormats.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Navigation;
+
+namespace ConvertWithMe.Core.Definitions
+{
+    public static class AudioFormats
+    {
+        public static Format mp3 = new Format()
+        {
+            videoCodecs = [],
+            audioCodecs = [AudioCodecs.mp3],
+            extension = "mp3"
+        };
+
+        public static Format aac = new Format()
+        {
+            videoCodecs = [],
+            audioCodecs = [AudioCodecs.aac],
+            extension = "aac"
+        };
+
+        public static Format flac = new Format()
+        {
+            videoCodecs = [],
+            audioCodecs = [AudioCodecs.flac],
+            extension = "flac"
+        };
+
+        public static Format m4a = new Format()
+        {
+            videoCodecs = [],
+            audioCodecs = [AudioCodecs.aac],
+            extension = "m4a"
+        };
+
+        public static Format ogg = new Format()
+        {
+            videoCodecs = [],
+            audioCodecs = [AudioCodecs.vorbis, AudioCodecs.opus, AudioCodecs.flac],
+            extension = "ogg"
+        };
+
+
+    }
+}
--- ConvertWithMe.Core/Definitions/VideoFormats.cs
+++ ConvertWithMe.Core/Definitions/VideoFormats.cs
@@ -3,9 +3,37 @@
     public static class VideoFormats
     {
         public static Format mp4 = new Format() { 
-            videoCodecs = [VideoCodecs.h264, VideoCodecs.h265], 
-            audioCodecs = [AudioCodecs.mp3, AudioCodecs.flac], 
+            videoCodecs = [VideoCodecs.h264, VideoCodecs.h265, VideoCodecs.mpeg4, VideoCodecs.mpeg2, VideoCodecs.mpeg1], 
+            audioCodecs = [AudioCodecs.mp3, AudioCodecs.aac],
             extension = "mp4" 
+        };
+
+        public static Format wmv = new Format()
+        {
+            videoCodecs = [VideoCodecs.wmv],
+            audioCodecs = [AudioCodecs.wmav2],
+            extension = "wmv"
+        };
+
+        public static Format flv = new Format()
+        {
+            videoCodecs = [VideoCodecs.flv1, VideoCodecs.h264],
+            audioCodecs = [AudioCodecs.mp3, AudioCodecs.aac],
+            extension = "flv"
+        };
+
+        public static Format mkv = new Format()
+        {
+            videoCodecs = [VideoCodecs.h264, VideoCodecs.h265, VideoCodecs.mpeg4, VideoCodecs.mpeg2, VideoCodecs.mpeg1],
+            audioCodecs = [AudioCodecs.mp3, AudioCodecs.aac, AudioCodecs.flac, AudioCodecs.vorbis],
+            extension = "mkv"
+        };
+
+        public static Format m4v = new Format()
+        {
+            videoCodecs = [VideoCodecs.h264],
+            audioCodecs = [AudioCodecs.aac],
+            extension = "m4v"
         };
     }
 }
