4f67e2ad7c8a271e26fd13203be47c27d8a7b075
Jananas <jan.gellbach@gmail.com>
2024-12-10 19:01:26 +0100

    Adding Add and remove functionality

--- ConvertWithMe.UI/ViewModels/ItemListViewModel.cs
+++ ConvertWithMe.UI/ViewModels/ItemListViewModel.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ConvertWithMe.Core;
+using ConvertWithMe.UI.Models;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using Xabe.FFmpeg;
 
 namespace ConvertWithMe.UI.ViewModels
 {
-    class ItemListViewModel
+    partial class ItemListViewModel : ObservableObject
     {
+
+        private ObservableCollection<FileItem> fileItems;
+        public IEnumerable<FileItem> FileItems => fileItems;
+
+        public ItemListViewModel()
+        {
+            if (!ApplicationPaths.FFmpeg.Equals(FFmpeg.ExecutablesPath))
+            {
+                FFmpeg.SetExecutablesPath(ApplicationPaths.FFmpeg);
+            }
+
+            fileItems = new ObservableCollection<FileItem>();
+        }
+
+        [RelayCommand]
+        public async Task AddFiles()
+        {
+            OpenFileDialog dialog = new OpenFileDialog() 
+            { 
+                Multiselect = true,
+                DefaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            };
+            if (dialog.ShowDialog() ?? false)
+            {
+                for (int i = 0; i < dialog.FileNames.Length; i++)
+                {
+                    string filename = dialog.FileNames[i];
+                    // Skip existing files
+                    if (fileItems.Where(x => x.SettingsFile.FileSrc.FullName.Equals(filename)).Any()) { continue; }
+
+                    SettingsFile settingsFile = new SettingsFile(filename, filename);
+                    IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(filename);
+
+                    FileItem item = new FileItem(
+                        settingsFile, 
+                        new SettingsMetadata(), 
+                        new SettingsVideo(), 
+                        new SettingsAudio(), 
+                        mediaInfo.AudioStreams.FirstOrDefault(), 
+                        mediaInfo.VideoStreams.FirstOrDefault()
+                        );
+
+                    fileItems.Add(item);
+                }
+            }
+        }
+
+        [RelayCommand]
+        public void RemoveItem(string fileSrc)
+        {
+            FileItem? toRemove = fileItems.Where(x => x.SettingsFile.FileSrc.FullName.Equals(fileSrc)).FirstOrDefault();
+            if (toRemove == null)
+            {
+                throw new KeyNotFoundException($"No element with path {fileSrc}");
+            }
+            fileItems.Remove(toRemove);
+        }
+
+        private SettingsMetadata ReadMetadata(string file)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
