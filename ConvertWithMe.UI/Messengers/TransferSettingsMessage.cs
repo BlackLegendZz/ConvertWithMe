931d1d169cd66afba9348f287c7beedb8524d13d
Jananas <jan.gellbach@gmail.com>
2024-12-12 22:10:49 +0100

    Add a message to communicate settings between VMs

---
+++ ConvertWithMe.UI/Messengers/TransferSettingsMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using ConvertWithMe.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertWithMe.UI.Messengers
+{
+    public class TransferSettingsMessage : ValueChangedMessage<ISettings?>
+    {
+        public TransferSettingsMessage(ISettings? value) : base(value)
+    {
+    }
+}
+}
