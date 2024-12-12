168351e97f4905d1964503ce116aa3cd231ffa79
Jananas <jan.gellbach@gmail.com>
2024-12-12 22:10:22 +0100

    Group VMS with an interface

---
+++ ConvertWithMe.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertWithMe.UI.ViewModels
+{
+    class MainWindowViewModel : ObservableObject, IViewModel
+    {
+        
+    }
+}
