01ab5a9699825c3b9995ee5fd98d000e69463d43
Jananas <jan.gellbach@gmail.com>
2024-12-12 22:12:49 +0100

    Syncronise Settings between VMs and add first simple bindings

--- ConvertWithMe.UI/ViewModels/SettingsViewModel.cs
+++ ConvertWithMe.UI/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using ConvertWithMe.UI.Messengers;
 using ConvertWithMe.UI.Models;
 using Microsoft.Win32;
+using System.CodeDom;
 
 namespace ConvertWithMe.UI.ViewModels
 {
-    partial class SettingsViewModel : ObservableObject
+    partial class SettingsViewModel : ObservableObject, IViewModel
     {
         [ObservableProperty]
         private bool displayMetadata = true;
@@ -23,18 +26,58 @@ namespace ConvertWithMe.UI.ViewModels
         private bool displayAudio = false;
 
         [ObservableProperty]
-        private SettingsFile? settingsFile;
+        private SettingsFile? sFile;
 
         [ObservableProperty]
-        private SettingsMetadata? settingsMetadata;
+        private SettingsMetadata? sMetadata;
 
         [ObservableProperty]
-        private SettingsVideo? settingVideo;
+        private SettingsVideo? sVideo;
 
         [ObservableProperty]
-        private SettingsAudio? settingsAudio;
+        private SettingsAudio? sAudio;
 
-        [RelayCommand]
+		public SettingsViewModel()
+		{
+            WeakReferenceMessenger.Default.Register<TransferSettingsMessage>(this, (r, m) => {
+                ISettings? settings = m.Value;
+               
+                if (settings == null)
+                {
+                    SFile = null;
+                    SMetadata = null;
+                    SVideo = null;
+                    SAudio = null;
+                    return;
+                }
+                Type sType = settings.GetType();
+
+                if (sType == typeof(SettingsFile))
+                {
+                    SFile = settings as SettingsFile;
+                    return;
+                }
+                if (sType == typeof(SettingsMetadata))
+                {
+                    SMetadata = settings as SettingsMetadata;
+                    return;
+                }
+                if (sType == typeof(SettingsVideo))
+                {
+                    SVideo = settings as SettingsVideo;
+                    return;
+                }
+                if (sType == typeof(SettingsAudio))
+                {
+                    SAudio = settings as SettingsAudio;
+                    return;
+                }
+
+                throw new ArgumentException($"The provided settings class of type {sType.FullName} is invalid.");
+            });
+		}
+
+		[RelayCommand]
         public void UpdateDestination()
         {
             OpenFolderDialog dialog = new OpenFolderDialog()
@@ -44,9 +87,11 @@ namespace ConvertWithMe.UI.ViewModels
             };
             if (dialog.ShowDialog() ?? false)
             {
-                settingsFile.DirDest = dialog.SafeFolderName;
+                SFile.DirDest = dialog.SafeFolderName;
             }
         }
+
+
 
         [RelayCommand]
         public void UpdateFilename()
@@ -54,9 +99,6 @@ namespace ConvertWithMe.UI.ViewModels
 
         }
 
-        public SettingsViewModel()
-        {
 
-        }
     }
 }
