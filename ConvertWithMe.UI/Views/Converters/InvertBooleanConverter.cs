6bbb97cb20fde989b3fe21c6609c488e3d4bc44e
Jananas <jan.gellbach@gmail.com>
2024-12-22 17:30:53 +0100

    new converter

---
+++ ConvertWithMe.UI/Views/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ConvertWithMe.UI.Views.Converters
+{
+    class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() != typeof(bool))
+            {
+                throw new ArgumentException($"value is of type {value.GetType()} and not bool");
+            }
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
