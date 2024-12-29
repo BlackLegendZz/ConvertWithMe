eccb567ebadf400e804d149baf2925a6c9980e91
Jananas <jan.gellbach@gmail.com>
2024-12-29 14:58:47 +0100

    removed navPanel and dynamically divide displayed bitrate

---
+++ ConvertWithMe.UI/Views/Converters/DivideConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ConvertWithMe.UI.Views.Converters
+{
+    class DivideConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal num = System.Convert.ToDecimal(value);
+            decimal num2 = System.Convert.ToDecimal(parameter);
+            return Math.Round(num / num2, 2);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
